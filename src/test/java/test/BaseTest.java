bbb837bb30b10d96aac51671a1d77aa78a3cbb58
Sergey Evseenko <evseenkosv@tut.by>
2021-01-09 21:05:05 +0300

    Updated driver settings.

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,13 +1,15 @@
 package test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.*;
-import utils.CapabilitiesGenerator;
 import utils.PropertyManager;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
@@ -17,15 +19,22 @@ public abstract class BaseTest {
     CreateVoiceTalentPage createVoiceTalentPage;
     CreateVoiceSamplePage createVoiceSamplePage;
     MainPage mainPage;
-    String id, password, url, filePath;
+    String id, password, url;
+    String filePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "QA.mp3";
 
     private WebDriver driver;
 
     @BeforeClass(description = "Opening Chrome Driver")
     public void setDriver() {
-        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+
+
         loginPage = new LoginPage(driver);
         googleSignInPage = new GoogleSignInPage(driver);
         createVoiceTalentPage = new CreateVoiceTalentPage(driver);
@@ -35,13 +44,14 @@ public abstract class BaseTest {
         id = props.get("id");
         password = props.get("password");
         url = props.get("url");
-        filePath = props.get("filePath");
-
     }
+
 
     @AfterClass(description = "Closing Chrome Driver", alwaysRun = true)
     public void closeDriver() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 }
