47778c8199c25ce18b175fab02785dc044e9218d
Sergey Evseenko <evseenkosv@tut.by>
2021-01-09 21:28:27 +0300

    Updated driver settings.

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -29,6 +29,12 @@ public abstract class BaseTest {
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
         options.setHeadless(false);
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
