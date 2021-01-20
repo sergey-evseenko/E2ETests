27f2592f6d9be970ecbed44f28e5206ec274dc6b
Sergey Evseenko <evseenkosv@tut.by>
2021-01-21 00:21:43 +0300

    Updated waits.

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,9 +1,7 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
@@ -12,7 +10,7 @@ public abstract class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(this.driver, 10);
+        wait = new WebDriverWait(this.driver, 60);
         PageFactory.initElements(driver, this);
     }
 
@@ -20,13 +18,4 @@ public abstract class BasePage {
 
     public abstract BasePage openPage(String url);
 
-    public void selectValue(String id, String value) {
-
-        String locatorForClick = String.format("//input[@id='%s']//..//..", id);
-        driver.findElement(By.xpath(locatorForClick)).click();
-        //String locatorForSelect = String.format("//div[@label='%s']", value);
-        String locatorForSelect = String.format("//div[@title='%s']", value);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locatorForSelect)));
-        driver.findElement(By.xpath(locatorForSelect)).click();
-    }
 }
